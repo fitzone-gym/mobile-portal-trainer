98ccf1b7f88cfb8547a10abc92697333b01b202c
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-07-05 15:13:57 +0530

    trainer app index

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/home.style';
+
+import { View, Text, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+export default function Home() {
+    const router = useRouter()
+    return (
+        <SafeAreaView>
+            <Stack.Screen
+                options={{
+                    title: 'Home',
+                }}
+            />
+            
+            <ScrollView>
+                <View
+                style={styles.container}
+                >
+                    <Text
+                        style={styles.heading}
+                    >Home</Text>
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() => {
+                            router.push('/member')
+                        }}
+                    >
+                        <Text>Click</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            
+        </SafeAreaView>
+    )
+}
