7e9c21e4ee9c17eca223f8db7cc63f38414828be
Dasuni Dewani <dasunidewani1998@gmail.com>
2023-08-03 11:39:03 +0530

    delete sidebar

--- app/TrainerSignup/index.tsx
+++ app/TrainerSignup/index.tsx
@@ -1,6 +1,8 @@
 
-import { View, Text, TouchableOpacity, SafeAreaView,Image, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView,Image, ImageBackground , TextInput} from 'react-native';
 import { Stack, useRouter } from 'expo-router';
+
+import styles from '../../styles/signup.style'
 
 export default function Home() {
     const router = useRouter()
@@ -11,6 +13,44 @@ export default function Home() {
                     title: 'Signup page',
                 }}
             />
+            
+            <View style={styles.container}>
+                <ImageBackground source= {require('../../assets/images/signin.png')}resizeMode='cover' style={styles.backgroundImage}>
+                    <View style={styles.form}>
+                        <Text style={styles.heading}>
+                            Sign Up
+                        </Text>
+
+                        <View style={styles.signinTxt}>
+                            <Text style={styles.subContent}>User Name</Text>
+                            <TextInput style={styles.txtInput}  />
+                            <Text style={styles.subContent}>Password</Text>
+                            <TextInput style={styles.txtInput}  secureTextEntry={true} />
+                        </View>    
+
+                        <TouchableOpacity
+                            style={styles.btn}
+                            onPress={() => {
+                                router.push('../TrainerDashboard')
+                            }}
+                        >
+                            <Text style={styles.btnTxt}>Sign in</Text>
+                        </TouchableOpacity>
+
+                        <View>
+                            <Text>Already have an account?</Text>
+                            <TouchableOpacity
+                                    onPress={()=>{
+                                        router.push('../TrainerSignin')
+                                    }}
+                                >
+                                    <Text style={styles.pwtxt}>Sign in</Text>
+                            </TouchableOpacity>
+                        </View>
+                        
+                    </View>
+                </ImageBackground>
+            </View>
         </SafeAreaView>
     )
 }
---
+++ styles/signup.style.js
@@ -0,0 +1,70 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+    
+    container: {
+        minHeight: '100%',       
+    }, 
+    backgroundImage:{
+        flex: 1,
+        justifyContent:'space-between',
+        height: '110%',
+        width: '100%',
+    },
+
+    form:{
+        alignItems: 'center',
+        backgroundColor: '#222222',
+        opacity: 0.9,
+        margin:30,
+        padding:0,
+        borderRadius:17,
+    },
+
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#E54646',
+        marginTop:10
+    },
+    signinTxt:{
+        justifyContent: 'center',
+        padding:20,
+        width:"100%",
+    },
+    subContent: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        textAlign:'left'
+    },
+    txtInput:{
+        fontSize: 14,
+        color: 'white',
+        padding:3,
+        borderBottomWidth: 1, 
+        borderColor: '#E54646',
+        marginBottom:30
+    },
+    pwtxt:{
+        color: '#E54646',
+        fontSize: 16,
+        textAlign: 'right',
+    },
+
+    btn: {
+        width: 250, 
+        height: 50,
+        marginBottom: 30,
+        backgroundColor: '#E54646',   
+        borderRadius: 30,
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+    btnTxt:{
+        fontSize: 23,
+        color: 'white',
+
+    },
+})
+
+export default styles;
